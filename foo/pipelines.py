83719853653b0e472d91b128036c8ba347e4aed2
gaodi <14841787@qq.com>
2023-06-29 17:39:45 +0800

    fix

--- foo/pipelines.py
+++ foo/pipelines.py
@@ -16,16 +16,16 @@ import pandas as pd
 
 class FooPipeline:
     def process_item(self, item, spider):
-        # pd_data = pd.read_csv(URL_PATH)
-        # if 'live' not in pd_data:
-        #     pd_data.loc[:, 'live'] = ''
-        # if 'last_live_check_date' not in pd_data:
-        #     pd_data.loc[:, 'last_live_check_date'] = ''
-        # if 'description' not in pd_data:
-        #     pd_data.loc[:, 'description'] = ''
-        # pd_data['live'].loc[item['index']] = 'Yes' if item['live'] else 'No'
-        # pd_data['last_live_check_date'].loc[item['index']] = datetime.datetime.now().strftime('%Y-%m-%d')
-        # if item['index'] < 10:
-        #     pd_data['description'].loc[item['index']] = item['job_description']
-        # pd_data.to_csv(URL_PATH, index=False)
+        pd_data = pd.read_csv(URL_PATH)
+        if 'live' not in pd_data:
+            pd_data.loc[:, 'live'] = ''
+        if 'last_live_check_date' not in pd_data:
+            pd_data.loc[:, 'last_live_check_date'] = ''
+        if 'description' not in pd_data:
+            pd_data.loc[:, 'description'] = ''
+        pd_data['live'].loc[item['index']] = 'Yes' if item['live'] else 'No'
+        pd_data['last_live_check_date'].loc[item['index']] = datetime.datetime.now().strftime('%Y-%m-%d')
+        if item['index'] < 10:
+            pd_data['description'].loc[item['index']] = item['job_description']
+        pd_data.to_csv(URL_PATH, index=False)
         return item
--- foo/utils/url_util.py
+++ foo/utils/url_util.py
@@ -7,17 +7,13 @@ import numpy as np
 
 
 def get_urls():
-    # pd_data = pd.read_csv(URL_PATH)
-    # if 'live' not in pd_data:
-    #     pd_data.loc[:, 'live'] = ''
-    # pd_data['live'] = pd_data['live'].replace(np.nan, '')
-    # for row in pd_data.itertuples():
-    #     if not getattr(row, 'live'):
-    #         yield {
-    #             'index': getattr(row, 'Index'),
-    #             'url': getattr(row, 'application_link_1')
-    #         }
-    yield {
-        'index': 2,
-        'url': 'https://lensa.com/information-systems-security-officer-isso-jobs/panama-city/jd/57abb2242aed0ef18297db35c25eb915?utm_campaign=google_jobs_apply&utm_source=google_jobs_apply&utm_medium=organic'
-    }
+    pd_data = pd.read_csv(URL_PATH)
+    if 'live' not in pd_data:
+        pd_data.loc[:, 'live'] = ''
+    pd_data['live'] = pd_data['live'].replace(np.nan, '')
+    for row in pd_data.itertuples():
+        if not getattr(row, 'live'):
+            yield {
+                'index': getattr(row, 'Index'),
+                'url': getattr(row, 'application_link_1')
+            }
